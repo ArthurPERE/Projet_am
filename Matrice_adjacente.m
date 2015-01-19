cd153df17fda906c04957b246c36a9fa8589f286
ArthurPERE <arthur.pere@insa-lyon.fr>
2015-01-19 14:42:55 +0100

    test

---
+++ Matrice_adjacente.m
@@ -0,0 +1,8 @@
+% Pour importé un fichier : uiimport
+% a regader : unique
+% uiimport
+
+
+A = unique(EntrezGeneInteractorA1); %creation d'un vecteur de unique pour les genes A1
+B = unique(EntrezGeneInteractorB1); %creation d'un vecteur de unique pour les genes B1
+
