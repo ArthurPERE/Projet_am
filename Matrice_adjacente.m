0e279cfbb58a15ac3d9d5f1445336ac563815e26
ArthurPERE <arthur.pere@insa-lyon.fr>
2015-01-20 18:26:06 +0100

    La premiere question est finie

--- Matrice_adjacente.m
+++ Matrice_adjacente.m
@@ -2,7 +2,31 @@
 % a regader : unique
 % uiimport
 
+A = EntrezGeneInteractorA;
+B = EntrezGeneInteractorB;
 
-A = unique(EntrezGeneInteractorA1); %creation d'un vecteur de unique pour les genes A1
-B = unique(EntrezGeneInteractorB1); %creation d'un vecteur de unique pour les genes B1
+A1 = unique(A); %creation d'un vecteur d'unique pour les genes A
+B1 = unique(B); %creation d'un vecteur d'unique pour les genes B
 
+ones = unique([A1;B1]);  %creation d'un vecteur d'unique pour les 2 genes
+
+M_A= zeros(length(ones)-2); %création d'une matrice M_A remplie de 0 de coté = a la longueur de ones -2 car les deux derniers termes de ones sont NA 
+
+z = find(A == ones(1));
+
+length(z);
+
+
+for i = 1 : 1 : length(ones)-2
+    z1 = find(A == ones(i)); %chercher les indices dans A des valeurs uniques de ones
+    z2 = find(B == ones(i)); %chercher les indices dans B des valeurs uniques de ones
+    
+    if ~isempty(z1) && ~isempty(z2)  %Si il y au moins une valeur qu'on cherche dans A et dans B
+        for t = 1 : 1 : length(z1) 
+            f =find(ones == B( z1(t) )); %chercher la valeur de associée a B(z1(t)) dans le vecteur ones pour la coordonée j dans la matrice final
+            M_A( i, f(1) ) = 1 + M_A( i, f(1) ); %pour mettre les valeur dans la matrice
+            M_A( f(1),i ) = 1 + M_A( f(1),i ); %pour mettre les valeurs dans la partie haute de la matrice
+        end
+    end
+    
+end;
